361bf956d6381d5a6a1bf6ed229d92ff7018029b
DangQuyHung <quyhung0121@gmail.com>
2025-03-12 15:41:56 +0700

    Add progress bar feature

--- src/routes/svg.route.js
+++ src/routes/svg.route.js
@@ -1,5 +1,7 @@
 import express from "express";
 import { progressCircle } from "../controller/svg.controller.js";
+import { progressBar } from "../controller/svg.controller.js";
 const router = express.Router();
 router.get('/progress', progressCircle);
+router.get('/progressbar', progressBar);
 export default router;
