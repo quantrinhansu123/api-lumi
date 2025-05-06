07dc6879c15a9bf8d78cf15cb7d0ccff6ad53216
nghia64 <nguyentrongn168@gmail.com>
2025-05-06 16:05:23 +0700

    fix

--- src/controller/qr.controller.js
+++ src/controller/qr.controller.js
@@ -5,22 +5,60 @@ export const getQR = async (req, res) => {
     try {
         const { orderId, amount } = req.query;
 
-        const data = await axios.post(`https://core.devops.sieutho.vn/external/deposit`,
+        // Gửi request tạo mã QR
+        const { data } = await axios.post(`https://core.devops.sieutho.vn/external/deposit`,
             {
-
-                "OrderId": orderId,
-
-                "Amount": amount
-
-            }, {
-            headers: {
-                "api-key": process.env.QR_KEY,
-                "Content-Type": `application/json`,
+                OrderId: orderId,
+                Amount: amount
+            },
+            {
+                headers: {
+                    "api-key": process.env.QR_KEY,
+                    "Content-Type": "application/json",
+                }
             }
-        }
-        )
+        );
 
-        const svg = await generateQR(data?.data?.data);
+        const qrText = data?.data;
+
+        // Thực hiện song song:
+        const [_, svg] = await Promise.all([
+            // Gửi request cập nhật vào AppSheet
+            axios.post(`https://www.appsheet.com/api/v2/apps/18f892a1-82e5-4731-a85a-8cad999d11f2/tables/${app.table}/Action`, {
+                Action: "Edit",
+                Properties: {},
+                Rows: [
+                    {
+                        Id: orderId,
+                        qrText: qrText,
+                    }
+                ]
+            }, {
+                headers: {
+                    "ApplicationAccessKey": process.env.APPSHEET_KEY,
+                    "Content-Type": "application/json",
+                }
+            }),
+
+            // Tạo SVG QR code
+            generateQR(qrText)
+        ]);
+
+        res.setHeader('Content-Type', 'image/svg+xml');
+        res.send(svg);
+
+    } catch (err) {
+        console.error(err);
+        res.status(500).send('Lỗi tạo QR');
+    }
+};
+
+
+export const getQRText = async (req, res) => {
+    try {
+        const { text } = req.query;
+
+        const svg = await generateQR(text);
 
         res.setHeader('Content-Type', 'image/svg+xml');
         res.send(svg);
--- src/routes/qr.router.js
+++ src/routes/qr.router.js
@@ -1,7 +1,8 @@
 import express from "express";
-import { getQR } from "../controller/qr.controller.js";
+import { getQR, getQRText } from "../controller/qr.controller.js";
 const router = express.Router();
 router.get('/', getQR);
+router.get('/text', getQRText);
 export default router;
 
 
