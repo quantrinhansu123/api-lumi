9057ac2d0b6faa1bd56aba05f5a3e400232e1bc2
nghia64 <nguyentrongn168@gmail.com>
2025-05-06 16:28:39 +0700

    fix

--- src/controller/qr.controller.js
+++ src/controller/qr.controller.js
@@ -5,54 +5,50 @@ export const getQR = async (req, res) => {
     try {
         const { orderId, amount } = req.query;
 
-        // Gửi request tạo mã QR
-        const { data } = await axios.post(`https://core.devops.sieutho.vn/external/deposit`,
+        const data = await axios.post(`https://core.devops.sieutho.vn/external/deposit`,
             {
-                OrderId: orderId,
-                Amount: amount
-            },
-            {
-                headers: {
-                    "api-key": process.env.QR_KEY,
-                    "Content-Type": "application/json",
-                }
-            }
-        );
 
-        const qrText = data?.data?.data;
+                "OrderId": orderId,
 
-        // Thực hiện song song:
-        const [_, svg] = await Promise.all([
-            // Gửi request cập nhật vào AppSheet
-            axios.post(`https://www.appsheet.com/api/v2/apps/18f892a1-82e5-4731-a85a-8cad999d11f2/tables/${app.table}/Action`, {
-                Action: "Edit",
-                Properties: {},
-                Rows: [
-                    {
-                        Id: orderId,
-                        qrText: qrText,
-                    }
-                ]
+                "Amount": amount
+
             }, {
-                headers: {
-                    "ApplicationAccessKey": process.env.APPSHEET_KEY,
-                    "Content-Type": "application/json",
-                }
-            }),
+            headers: {
+                "api-key": process.env.QR_KEY,
+                "Content-Type": `application/json`,
+            }
+        }
+        )
 
-            // Tạo SVG QR code
-            generateQR(qrText)
-        ]);
+        await axios.post(`https://www.appsheet.com/api/v2/apps/18f892a1-82e5-4731-a85a-8cad999d11f2/tables/Đơn mua booking/Action`, {
+            "Action": "Edit",
+            "Properties": {
+                "Usersettings": {
+                    "info": "Admin"
+                }
+            },
+            "Rows": [
+                {
+                    "Id": orderId,
+                    "qrText": `https://n-api-rouge.vercel.app/qr/text?text=${data?.data?.data}`,
+                }
+            ]
+        }, {
+            headers: {
+                "ApplicationAccessKey": `${process.env.APPSHEET_KEY}`,
+                "Content-Type": `application/json`,
+            }
+        })
+
+        const svg = await generateQR(data?.data?.data);
 
         res.setHeader('Content-Type', 'image/svg+xml');
         res.send(svg);
-
     } catch (err) {
         console.error(err);
         res.status(500).send('Lỗi tạo QR');
     }
 };
-
 
 export const getQRText = async (req, res) => {
     try {
